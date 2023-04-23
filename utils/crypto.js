a924c159072aae56cd50299aad9428c4ea2f8e3d
wy <2514825513@qq.com>
2023-04-23 13:03:28 +0800

    ADD: add content-range

--- utils/crypto.js
+++ utils/crypto.js
@@ -5,5 +5,5 @@ let r2 = crypto.createHash('md5').update('a').update('b').update('cd').digest('b
 console.log(r1,r2)//true
 // 2. 加盐算法 （盐值，秘钥）
 // 可以把秘钥生成一个1k大小的 随机的字符，在用作秘钥  jwt的原理
-let r3 = crypto.createHmac('sha256','zf1').update('abcd').digest('base64')
+let r3 = crypto.createHmac('sha256','wy1').update('abcd').digest('base64')
 console.log(r3)
